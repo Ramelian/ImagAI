a2f98b31448a2eb6bf10bdd271cbe0fdad87534e
Ramelian <123Ramin321@gmail.com>
2024-04-13 23:42:08 +0200

    removed bug from Search

---
+++ types/index.d.ts
@@ -0,0 +1,12 @@
+declare type CreateUserType = {
+    clerkId: string;
+    email: string;
+    username: string;
+    photo: string;
+    firstName?: string;
+    lastName?: string;
+}
+
+declare type UpdateUserType = {
+    
+}
